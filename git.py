36b29d3b6daf1d5db41b0af5ff5afe620d387d9b
Hassan Naik <hassannaik1989@gmail.com>
2024-10-04 10:02:00 +0100

    first commit

---
+++ git.py
@@ -0,0 +1,7 @@
+# echo "# DAWeek3" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/HassanNaik/DAWeek3.git
+# git push -u origin main
